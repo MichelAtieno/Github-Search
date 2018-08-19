2bade3c293f828c6ef2b0acb192b33a4f44d9e21
Michel Atieno <mishqamish@gmail.com>
2018-08-19 23:02:54 +0300

    Added remaining variables and methods to interface and promise method

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -32,12 +32,27 @@ export class ProfileService {
       // tslint:disable-next-line:max-line-length
       this.http.get<ApiResponse>('https://api.github.com/users/MichelAtieno?access_token=58b6bc80382e7bff719ab38e773fbddca03899b0').toPromise().then(getUserInfo => {
         this.user.name = getUserInfo.name;
+        this.user.login = getUserInfo.login;
         this.user.avatar_url  = getUserInfo.avatar_url;
+        this.user.html_url = getUserInfo.html_url;
+        this.user.public_repos = getUserInfo.public_repos;
+        this.user.hireable = getUserInfo.hireable;
+        this.user.created_at = getUserInfo.created_at;
+        this.user.followers = getUserInfo.followers;
+        this.user.following = getUserInfo.following;
         resolve();
       }, error => {
         console.log(' Failed.');
         this.user.name = 'Error';
+        this.user.login = 'Error';
         this.user.avatar_url = '';
+        this.user.html_url = 'https://github.com';
+        this.user.public_repos = 0;
+        this.user.hireable = false;
+        this.user.created_at = new Date(0, 0, 0 );
+        this.user.followers = 0;
+        this.user.following = 0;
+        reject(error);
       });
     });
     return promise;
