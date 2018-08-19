410d04384df546434dea643346148508034ee994
Michel Atieno <mishqamish@gmail.com>
2018-08-19 23:22:06 +0300

    Success in displaying User Info, Added variables in class repo

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,13 @@
 export class Repository {
+    constructor(
+        public name: string,
+        public html_url: string,
+        public created_at: Date,
+        public description: string
+         ) {
+             this.name = name;
+             this.html_url = html_url;
+             this.created_at = created_at;
+             this.description = description;
+         }
 }
