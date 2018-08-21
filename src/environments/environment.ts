0276146a15d0cc6ab9bc9a0c016c88cad30fa319
Michel Atieno <mishqamish@gmail.com>
2018-08-21 17:51:09 +0300

    Added css

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   accesstoken: '58b6bc80382e7bff719ab38e773fbddca03899b0',
 };
 
